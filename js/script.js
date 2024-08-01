6d1cf939fa9e4ca42619a1103a93b4864567eff3
Pasha <pasagalatov96@gmail.com>
2024-08-01 16:27:55 +0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let admin;
+let name;
+name = 'John';
+admin = name;
+alert (admin);
+
+const planetName = 'Oasis';
+const currentVisitorName = 'John';
+console.log('Oasis', planetName);
+console.log ('John', currentVisitorName);
+
+let variable = "10" + 10;
+console.log(variable);
+console.log(typeof variable);
