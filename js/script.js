8f40e49548b97018c1cfcb0d477ef1a7beb23c33
Pasha <pasagalatov96@gmail.com>
2024-08-02 10:52:36 +0300

    finished task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let value1 = parseFloat(prompt('Введіть перше число:'));
+let value2 = parseFloat(prompt('Введіть друге число:'));
+
+let sumResult = value1 + value2;
+console.log(`Результат: ${value1} + ${value2} = ${sumResult}`);
+
+let subResult = value1 - value2;
+console.log(`Результат: ${value1} - ${value2} = ${subResult}`);
+
+let mulResult = value1 * value2;
+console.log(`Результат: ${value1} * ${value2} = ${mulResult}`);
+
+let divResult = value1 / value2;
+console.log(`Результат: ${value1} / ${value2} = ${divResult}`);
