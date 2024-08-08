32f148f88d37be4a31c513008a65298a3d0ed7e1
Pasha <pasagalatov96@gmail.com>
2024-08-08 09:43:40 +0300

    Done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const name = prompt('Enter user name:');
+const age = prompt('Enter user age:');
+const likesProgramming = confirm ('Whether you enjoy programming?');
+console.log(name, age, likesProgramming);
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(likesProgramming));
+const greeting = (`Hey ${name}`);
+alert(greeting);
+const numericAge = Number(age);
+let nextAge = 1 + numericAge;
+console.log(nextAge);
+const isAdult = 18 <=age;
+console.log(isAdult)
+
+
+
