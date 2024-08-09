7379856ff4165b360c2d0c9c9ddc37f4783e417f
Pavlo <galatovpasa82@gmail.com>
2024-08-09 13:19:48 +0300

    finished task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const num1 = parseFloat(prompt('Введіть перше число:'));
+const num2 = parseFloat(prompt('Введіть друге число:'));
+const num3 = parseFloat(prompt('Введіть третє число:'));
+const average = (num1 + num2 + num3) / 3;
+alert('Середнє арифметичне цих чисел: ' + average);
