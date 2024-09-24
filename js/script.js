0e6c367e30828762cae44622cd2b7147a824f56a
Pavlo <galatovpasa82@gmail.com>
2024-09-24 13:04:52 +0300

    Finished task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function createSumFunction() {
+    let total = 0;
+
+    return function sum(value) {
+        total += value;
+        return total;
+    };
+}
+const sum = createSumFunction();
+
+console.log(sum(3));
+console.log(sum(7));
+console.log(sum(20));
