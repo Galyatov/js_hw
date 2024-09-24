7a158adf8a0260cab61a1ab937358046bd362304
Pavlo <galatovpasa82@gmail.com>
2024-09-24 16:40:35 +0300

    Finished task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const insertWordInto = (initialString) => {
+    const words = initialString.split(' ');
+    let currentPosition = 0;
+
+    return (newWord) => {
+        if (words.length === 0 || initialString.trim() === '') {
+            return newWord;
+        }
+
+        let result = [...words];
+
+        if (currentPosition === 0) {
+            result.unshift(newWord);
+        } else if (currentPosition === words.length) {
+            result.push(newWord);
+        } else {
+            result.splice(currentPosition, 0, newWord);
+        }
+
+        currentPosition = (currentPosition + 1) % (words.length + 1);
+
+        return result.join(' ');
+    };
+};
+
+const insert = insertWordInto('hello world');
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
+console.log(insert('Odesa'));
