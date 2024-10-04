0c521ab8a27f4fec3179c03125f39fdfcf1922a3
Pavlo <galatovpasa82@gmail.com>
2024-10-04 17:11:11 +0300

    Finished task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+
+const textField = document.getElementById('textField');
+const sideDiv = document.getElementById('sideDiv');
+
+textField.addEventListener('focus', function() {
+    sideDiv.style.display = 'block';
+});
+
+textField.addEventListener('blur', function() {
+    sideDiv.style.display = 'none';
+});
