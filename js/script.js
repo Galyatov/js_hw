154ce4aecd2f074f59296c27a066ede2fbaf2fa4
Pavlo <galatovpasa82@gmail.com>
2024-10-04 23:07:14 +0300

    Finished task

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 'use strict';
 
 const IMG_FOLDER = './images';
-const IMG_EXT = '.jpg'
-const appentHTMLelement = (parentElement = null, elementToAppend = null) => {
+const IMG_EXT = '.jpg';
+
+const generateImgElement = (imgName) => {
+    const img = document.createElement('img');
+    img.src = `${IMG_FOLDER}/${imgName}${IMG_EXT}`;
+    img.alt = `Image: ${imgName}`;
+    img.title = `Image: ${imgName}`;
+    return img;
+};
+
+const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const appendHTMLElement = (parentElement, elementToAppend) => {
     if (!parentElement || !elementToAppend) return;
     parentElement.append(elementToAppend);
 };
 
-const getRandomNumber = () => {
-    const num = Math.floor(Math.random() *9);
-    if (num === 0) return 1;
-    return num;
-};
 
-const generateImg = (imgName) => {
-    const img = document.createElement('img');
-    img.src = `${IMG_FOLDER}/${imgName + IMG_EXT}`;
-    img.alt = `${IMG_FOLDER}/${imgName + IMG_EXT}`;
-    img.title = `${IMG_FOLDER}/${imgName + IMG_EXT}`;
-    return img;
-};
-
-
-const imgToAppend = generateImg(getRandomNumber());
-appentHTMLelement(document.body, imgToAppend);
+const randomImgName = getRandomNumber(1, 9);
+const imgElement = generateImgElement(randomImgName);
+appendHTMLElement(document.body, imgElement);
