cdc7754a95ecc5fe9ded170ea896b1d5c2e0aebc
Pavlo <galatovpasa82@gmail.com>
2024-10-05 21:56:03 +0300

    Finished task

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
 'use strict';
 
-const textField = document.getElementById('textField');
-const sideDiv = document.getElementById('sideDiv');
+function createElements() {
+    const div = document.createElement('div');
+    div.style.display = 'flex';
 
-textField.addEventListener('focus', function() {
-    sideDiv.style.display = 'block';
-});
+    const textInput = document.createElement('input');
+    textInput.type = 'text';
+    textInput.style.marginRight = '10px';
 
-textField.addEventListener('blur', function() {
+    const sideDiv = document.createElement('div');
     sideDiv.style.display = 'none';
-});
+    sideDiv.innerHTML = 'Hala Madrid';
+
+    return { div, textInput, sideDiv };
+}
+
+function appendElementsToPage(elements) {
+    elements.div.appendChild(elements.textInput);
+    elements.div.appendChild(elements.sideDiv);
+    document.body.appendChild(elements.div);
+}
+
+function addEventListeners(elements) {
+    elements.textInput.addEventListener('focus', () => {
+        elements.sideDiv.style.display = 'block';
+    });
+
+    elements.textInput.addEventListener('blur', () => {
+        elements.sideDiv.style.display = 'none';
+    });
+}
+
+const elements = createElements();
+appendElementsToPage(elements);
+addEventListeners(elements);
