bdeef2adc608eef83ea58465fb78f59e900852c9
Pavlo <galatovpasa82@gmail.com>
2024-10-28 16:08:01 +0200

    Finished task

--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
 'use strict';
 
 let user = {
+    _name: 'Invisible',
+
+    _age: 0,
+
     get name() {
         return this._name;
     },
 
-    set name(name) {
-        this._name = name;
+    set name(newName) {
+        this._name = newName;
     },
-
-    _age: 0,
 
     get age() {
         return this._age;
     },
 
-    set age(value) {
-        if (typeof value === 'number' && value > 0 && value < 120) {
-            this._age = value;
+    set age(newAge) {
+        if (typeof newAge === 'number' && newAge >= 0 && newAge <= 120) {
+            this._age = newAge;
         } else {
             console.error('This age is impossible for a person');
         }
     }
 };
 
-user.name = "Pasha";
+user.name = 'Pasha';
 console.log(user.name);
 
 user.age = 21;
 console.log(user.age);
 
-user.age = 777;
+user.age = 130;
+
