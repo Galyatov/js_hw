00b512278d7e36367cd119085aa8462ac6258ed9
Pavlo <galatovpasa82@gmail.com>
2024-10-28 16:52:07 +0200

    Finished task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
 'use strict';
+
+let product = {
+    quantity: 0
+};
+Object.defineProperty(product, 'name', {
+    get: function() {
+        return this._name;
+    },
+    set: function(value) {
+        if (typeof value !== 'string' || value === '') {
+            console.log("Error: Name must be a non-empty string.");
+        } else {
+            this._name = value;
+        }
+    }
+});
+Object.defineProperty(product, 'price', {
+    get: function() {
+        return this._price;
+    },
+    set: function(value) {
+        if (typeof value !== 'number' || value <= 0) {
+            console.log("Error: Price must be a number greater than 0.");
+        } else {
+            this._price = value;
+        }
+    }
+});
+
+Object.defineProperty(product, 'totalValue', {
+    get: function() {
+        return this.price * this.quantity;
+    }
+});
+
+product.name = "Phone";
+product.price = 777;
+product.quantity = 9;
+
+console.log(product.name);
+console.log(product.price);
+console.log(product.totalValue);
+
+product.name = "";
+product.price = -9;
