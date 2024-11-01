1b8503bf58bfe1ed1df3df89707606809fb5ecda
Pavlo <galatovpasa82@gmail.com>
2024-11-01 11:22:04 +0200

    Finished task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function deepFreeze(obj) {
+    Object.keys(obj).forEach(key => {
+        if (typeof obj[key] === 'object' && obj[key] !== null) {
+            deepFreeze(obj[key]);
+        }
+    });
+    return Object.freeze(obj);
+}
+let user = {
+    data: {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: {
+            a1: 1,
+            b1: 2,
+            c1: 3,
+            d1: {
+                a2: 3,
+                b2: 3,
+                c2: 3,
+            }
+        },
+    }
+};
+
+deepFreeze(user);
+
+user.data.a = 17;
+console.log(user.data.a);
